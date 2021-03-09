60295b62e8bdbce67639935c432e02ef1481beea
Kir Sukhov <kirillsukhov@gmail.com>
2021-03-09 11:21:00 +0300

    2lesson

---
+++ 2task/2task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2task
+//
+//  Created by Kir S on 05.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
